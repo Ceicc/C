dfaf50dbe3bc2277787326cb559e2b800a64861a
husam <husam999h@gmail.com>
2022-07-02 09:46:45 +0300

    script for storing passwords

---
+++ appendPassword.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+char *getString(char *question);
+
+int main(void)
+{
+  FILE *f = fopen("/home/debian/passwords.csv", "a");
+
+  if (f == NULL)
+  {
+    fprintf(stderr, "There was an error while opening the passwords file\nError: %s\n", strerror(errno));
+    exit(errno);
+  }
+
+  char *website = getString("Website name: ");
+  char *username = getString("username: ");
+  char *password = getString("password: ");
+
+  fprintf(f, "%s,%s,%s\n", website, username, password);
+
+  free(website);
+  free(username);
+  free(password);
+
+}
+
+char *getString(char *question)
+{
+  char *line = NULL;
+  size_t lineLen = 0;
+  ssize_t read = 0;
+
+  printf("%s", question);
+
+  while ((read = getline(&line, &lineLen, stdin)) < 2)
+  {
+    if (read == -1)
+    {
+      fprintf(stderr, "Unexpectedly recived EOF");
+      exit(1);
+    }
+
+    printf("Value must be greater than two characters\n");
+    printf("%s", question);
+
+    line = NULL;
+    lineLen = 0;
+  }
+
+
+  if (line[strlen(line) - 1] == '\n')
+    line[strlen(line) - 1] = 0;
+
+  return line;
+}
